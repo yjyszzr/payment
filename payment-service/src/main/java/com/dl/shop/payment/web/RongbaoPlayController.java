f975720d482e88c36eceee657434efe21d02ebb3
123 <123@qq.com>
2018-05-03 15:56:31 +0800

    支付联调...

--- payment-service/src/main/java/com/dl/shop/payment/web/RongbaoPlayController.java
+++ payment-service/src/main/java/com/dl/shop/payment/web/RongbaoPlayController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.druid.support.logging.Log;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.dl.base.result.BaseResult;
@@ -53,7 +54,7 @@ public class RongbaoPlayController extends AbstractBaseController{
 	
 	@ApiOperation(value="融宝支付回调")
 	@PostMapping("callback")
-	public String payCallBack(HttpServletRequest request, HttpServletResponse response) {
+	public void payCallBack(HttpServletRequest request, HttpServletResponse response) {
 		String key = ReapalH5Config.key;
 		String merchantId = request.getParameter("merchant_id");
 		String data = request.getParameter("data");
@@ -87,17 +88,34 @@ public class RongbaoPlayController extends AbstractBaseController{
 					}else {
 						call(rEntity,payLog,request,response);
 					}
-					return "success";
+					try {
+						logger.info("返回success...");
+						response.getWriter().write("success");
+					} catch (IOException e) {
+						e.printStackTrace();
+					};
 				}else {
 					logger.info("验签失败...");
-					return "error";
+					try {
+						response.getWriter().write("error");
+					} catch (IOException e) {
+						e.printStackTrace();
+					};
 				}
 			}
 		}else {
 			logger.info("资金方回调参数错误 data:" + data +" encryptkey:" + encryptkey);
-			return "error";
+			try {
+				response.getWriter().write("error");
+			} catch (IOException e) {
+				e.printStackTrace();
+			};
 		}
-		return "error";
+		try {
+			response.getWriter().write("error");
+		} catch (IOException e) {
+			e.printStackTrace();
+		};
 	}
 	
 	/***
