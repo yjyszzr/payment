8e2577b6dec85e99c560656599964136bd9838a1
duanyu <duanyu@caixiaomi.com>
2018-05-06 10:48:37 +0800

    remove import

--- payment-service/src/main/java/com/dl/shop/payment/web/PaymentController.java
+++ payment-service/src/main/java/com/dl/shop/payment/web/PaymentController.java
@@ -11,8 +11,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.util.TextUtils;
 import org.slf4j.Logger;
@@ -24,8 +26,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.alibaba.fastjson.JSON;
-import com.dl.base.enums.PayEnum;
 import com.dl.base.result.BaseResult;
 import com.dl.base.result.ResultGenerator;
 import com.dl.base.util.DateUtil;
@@ -44,7 +46,6 @@ import com.dl.member.dto.UserBankDTO;
 import com.dl.member.dto.UserDTO;
 import com.dl.member.dto.UserRechargeDTO;
 import com.dl.member.dto.UserWithdrawDTO;
-import com.dl.member.enums.MemberEnums;
 import com.dl.member.param.AmountParam;
 import com.dl.member.param.IDParam;
 import com.dl.member.param.MessageAddParam;
@@ -60,11 +61,8 @@ import com.dl.order.param.SubmitOrderParam.TicketDetail;
 import com.dl.order.param.UpdateOrderInfoParam;
 import com.dl.shop.payment.dto.PayReturnDTO;
 import com.dl.shop.payment.dto.PaymentDTO;
-import com.dl.shop.payment.dto.RongbaoPayResultDTO;
 import com.dl.shop.payment.dto.RspOrderQueryDTO;
-import com.dl.shop.payment.dto.YinHeResultDTO;
 import com.dl.shop.payment.enums.PayEnums;
-import com.dl.shop.payment.model.OrderQueryResponse;
 import com.dl.shop.payment.model.PayLog;
 import com.dl.shop.payment.model.UnifiedOrderParam;
 import com.dl.shop.payment.model.UserWithdrawLog;
@@ -87,6 +85,7 @@ import com.dl.shop.payment.service.PayLogService;
 import com.dl.shop.payment.service.PayMentService;
 import com.dl.shop.payment.service.UserWithdrawLogService;
 import com.dl.shop.payment.utils.WxpayUtil;
+
 import io.swagger.annotations.ApiOperation;
 
 @Controller
