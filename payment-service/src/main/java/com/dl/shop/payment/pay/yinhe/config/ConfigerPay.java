b4c1ff471d1dcda8030c201be24cfed55ce5b04d
123 <123@qq.com>
2018-05-10 18:14:18 +0800

    修改秘钥...

--- payment-service/src/main/java/com/dl/shop/payment/pay/yinhe/config/ConfigerPay.java
+++ payment-service/src/main/java/com/dl/shop/payment/pay/yinhe/config/ConfigerPay.java
@@ -20,7 +20,7 @@ public class ConfigerPay {
 //	public static final String SECRET = "af9cc226dc9e70ae181b04c0ff181de7";
 //	public static final String SECRET = "77c21827d4725764696718349e5044d6";
 //	public static final String SECRET = "34884fd5f1d4eea10d3d410a44678e3f";
-	public static final String SECRET_PUBLIC = "77c21827d4725764696718349e5044d6";
+	public static final String SECRET_PUBLIC = "b4df7b2d0cb5a90659afcb165b701d5e";
 	public static final String SECRET = SECRET_PUBLIC;
 	
 	public static final String PAY_TYPE_WECHAT = "1";
