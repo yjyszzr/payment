1d6b980925d6967e1015ede8875b6246622b8f09
yjyszzr <1053195207@qq.com>
2018-05-11 22:38:08 +0800

    modified
    Change-Id: I0fb6a99018c0e74a306a71dbdbd4ac9fb3d695ea
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- payment-service/src/main/java/com/dl/shop/payment/web/UserWithdrawController.java
+++ payment-service/src/main/java/com/dl/shop/payment/web/UserWithdrawController.java
@@ -5,16 +5,13 @@ import com.dl.shop.payment.service.UserWithdrawService;
 import com.dl.base.result.BaseResult;
 import com.dl.base.service.AbstractService;
 import com.dl.member.dto.UserWithdrawDTO;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import javax.annotation.Resource;
 
-@Service
-@Transactional
+@Controller
 @RequestMapping("/payment/withdraw")
 public class UserWithdrawController extends AbstractService<UserWithdraw> {
     @Resource
