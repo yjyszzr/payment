73cdb72f5679438c494c31ebbf267ad49c1ddb3e
yjyszzr <1053195207@qq.com>
2018-05-11 22:49:13 +0800

    update controller to restcontryoller

--- payment-service/src/main/java/com/dl/shop/payment/web/UserWithdrawController.java
+++ payment-service/src/main/java/com/dl/shop/payment/web/UserWithdrawController.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 @RequestMapping("/payment/withdraw")
 public class UserWithdrawController extends AbstractService<UserWithdraw> {
     @Resource
