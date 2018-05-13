a86a40a9ad9e45ba79226e0e93c946c2a61d61d3
123 <123@qq.com>
2018-05-13 22:29:39 +0800

    url decode ...

--- payment-service/src/main/java/com/dl/shop/payment/web/PaymentController.java
+++ payment-service/src/main/java/com/dl/shop/payment/web/PaymentController.java
@@ -521,8 +521,8 @@ public class PaymentController extends AbstractBaseController{
 					try {
 						String qrCode = rYinHeEntity.qrCode;
 						encodeUrl = URLEncoder.encode(qrCode,"UTF-8");
-//						redirectUri = URLEncoder.encode(ConfigerPay.URL_REDIRECT+"?payLogId="+payLogId,"UTF-8");
-						redirectUri = ConfigerPay.URL_REDIRECT+"?payLogId="+payLogId;
+						redirectUri = URLEncoder.encode(ConfigerPay.URL_REDIRECT+"?payLogId="+payLogId,"UTF-8");
+//						redirectUri = ConfigerPay.URL_REDIRECT+"?payLogId="+payLogId;
 					} catch (UnsupportedEncodingException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
