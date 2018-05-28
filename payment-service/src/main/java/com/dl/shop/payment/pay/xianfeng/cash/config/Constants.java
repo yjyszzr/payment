b041f9e9a6ae73a9a5b5f07d65a6446fbb5c267f
123 <123@qq.com>
2018-05-28 12:41:18 +0800

    先锋正式号测试...

--- payment-service/src/main/java/com/dl/shop/payment/pay/xianfeng/cash/config/Constants.java
+++ payment-service/src/main/java/com/dl/shop/payment/pay/xianfeng/cash/config/Constants.java
@@ -1,13 +1,16 @@
 package com.dl.shop.payment.pay.xianfeng.cash.config;
 
 public class Constants {
-	public static String MER_ID = "M200000550";
+//	public static String MER_ID = "M200000550";
+	public static String MER_ID = "M200006743";
 	
-	public static String MER_RSAKEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQChFetx5+VKDoEXzZ+5Wozt3MfWMM/TiKMlWmAKXBViv8/e6j6SU/lSlWkMajd59aiWczs+qf9dMuRpe/l9Qke9DnVMn24JNLXjWD+y+w3yKRwd3CTtF7gx8/ToZl5XqFIT5YB1QfQCdAf8Z18IdQrJIijs8ssczY/RfqKZLo+KLQIDAQAB";
+//	public static String MER_RSAKEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQChFetx5+VKDoEXzZ+5Wozt3MfWMM/TiKMlWmAKXBViv8/e6j6SU/lSlWkMajd59aiWczs+qf9dMuRpe/l9Qke9DnVMn24JNLXjWD+y+w3yKRwd3CTtF7gx8/ToZl5XqFIT5YB1QfQCdAf8Z18IdQrJIijs8ssczY/RfqKZLo+KLQIDAQAB";
+	public static String MER_RSAKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoGpZSnEDar2rd4xzxeUMWQTXk+Mr8aLeGX2YJUpP+5HB/5IGDhit4IeglL8QU+dwZg4FEMrHH+AvxplR8Y7RNKlgZXVylFs9oRPQnNG+dJx2tWXAMXgeH+C4AA8KRkMlKo/PYLG3LasIk1eXyK2xc7LsMCVcfLjzEn99ua+JwG1ApTLXA9wcZ2naIOvwK3mevxaxasnqINQ96HBmwzJSXfz0ShIF6BAlFUlmFw1GLmQrChNUePZaSxsbAAPMuSOcvkbnKTLrKa3dXS6S23AZvpsN3cWAGLF36PFyo2oEXvcrOVM0Id/at4jolQKNf7NXt9Q9xJivXPvGJj3EFkaAWwIDAQAB";
 	
 	public static String SEC_ID = "RSA";
 	
-	public static String UCF_GATEWAY_URL = "http://sandbox.firstpay.com/security/gateway.do";
+//	public static String UCF_GATEWAY_URL = "http://sandbox.firstpay.com/security/gateway.do";
+	public static String UCF_GATEWAY_URL = "https://mapi.ucfpay.com/gateway.do";
 	
 //	public static String RETURN_URL = "http://1.2.7.1:8080/withdraw/ReceiveReturn";
 	
