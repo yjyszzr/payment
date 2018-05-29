65898cb782eb77b44e6e987bc5c227020791ff16
123 <123@qq.com>
2018-05-29 18:56:04 +0800

    先锋配置测试...

--- payment-service/src/main/java/com/dl/shop/payment/pay/xianfeng/cash/config/Constants.java
+++ payment-service/src/main/java/com/dl/shop/payment/pay/xianfeng/cash/config/Constants.java
@@ -10,25 +10,25 @@ public class Constants {
 	
 //	public static String MER_ID = "M200000550";
 	@Value("${xianfeng.app_merid}")
-	private String MER_ID = "M200006743";
+	private String MER_ID;
 	
 //	public static String MER_RSAKEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQChFetx5+VKDoEXzZ+5Wozt3MfWMM/TiKMlWmAKXBViv8/e6j6SU/lSlWkMajd59aiWczs+qf9dMuRpe/l9Qke9DnVMn24JNLXjWD+y+w3yKRwd3CTtF7gx8/ToZl5XqFIT5YB1QfQCdAf8Z18IdQrJIijs8ssczY/RfqKZLo+KLQIDAQAB";
 	@Value("${xianfeng.app_mer_rsakey}")
-	private String MER_RSAKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoGpZSnEDar2rd4xzxeUMWQTXk+Mr8aLeGX2YJUpP+5HB/5IGDhit4IeglL8QU+dwZg4FEMrHH+AvxplR8Y7RNKlgZXVylFs9oRPQnNG+dJx2tWXAMXgeH+C4AA8KRkMlKo/PYLG3LasIk1eXyK2xc7LsMCVcfLjzEn99ua+JwG1ApTLXA9wcZ2naIOvwK3mevxaxasnqINQ96HBmwzJSXfz0ShIF6BAlFUlmFw1GLmQrChNUePZaSxsbAAPMuSOcvkbnKTLrKa3dXS6S23AZvpsN3cWAGLF36PFyo2oEXvcrOVM0Id/at4jolQKNf7NXt9Q9xJivXPvGJj3EFkaAWwIDAQAB";
+	private String MER_RSAKEY;
 	
 	@Value("${xianfeng.app_secid}")
 	private String SEC_ID = "RSA";
 	
 //	public static String UCF_GATEWAY_URL = "http://sandbox.firstpay.com/security/gateway.do";
 	@Value("${xianfeng.app_ufc_gateway}")
-	private String UCF_GATEWAY_URL = "https://mapi.ucfpay.com/gateway.do";
+	private String UCF_GATEWAY_URL;
 	
 //	public static String RETURN_URL = "http://1.2.7.1:8080/withdraw/ReceiveReturn";
 	
 //	public static String NOTICE_URL = "http://39.106.18.39:7076/cash/notify";
 	@Value("${xianfeng.app_notice_url}")
-	private String NOTICE_URL = "http://api.caixiaomi.net/api/payment/cash/notify";
+	private String NOTICE_URL;
 	
 	@Value("${xianfeng.app_version}")
-	private String VERSION = "4.0.0";
+	private String VERSION;
 }
