b90537ebdf1403bdfbd42add01759b01d3cf7034
123 <123@qq.com>
2018-06-29 19:31:25 +0800

    提示文案修改

--- payment-service/src/main/java/com/dl/shop/payment/service/XianFengService.java
+++ payment-service/src/main/java/com/dl/shop/payment/service/XianFengService.java
@@ -78,7 +78,11 @@ public class XianFengService {
 				//主动query接口
 				PayManager.getInstance().addReqQueue(orderSn, payOrderSn, payCode);
 				return ResultGenerator.genSuccessResult("支付申请成功");
-			}else {
+			}else if(rspEntity.isVerfyCodeWrong()){
+				return ResultGenerator.genResult(PayEnums.PAY_XIANFENG_PAY_ERROR.getcode(),PayEnums.PAY_XIANFENG_PAY_ERROR.getMsg());
+			}else if(rspEntity.isVerifyCodeInValid()) {
+				return ResultGenerator.genResult(PayEnums.PAY_XIANFENG_VERIFYCODE_INVALID.getcode(),PayEnums.PAY_XIANFENG_VERIFYCODE_INVALID.getMsg());
+			}else{
 				return ResultGenerator.genResult(PayEnums.PAY_XIANFENG_PAY_ERROR.getcode(),PayEnums.PAY_XIANFENG_PAY_ERROR.getMsg() + "[" + rspEntity.resMessage +"]");
 			}
 		}
