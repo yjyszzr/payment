13dc1e56c5dc739b4a5f989871e4e3d7a9c28121
duanyu <duanyu@caixiaomi.com>
2018-07-03 14:18:39 +0800

    update

--- payment-api/src/main/java/com/dl/shop/payment/enums/PayEnums.java
+++ payment-api/src/main/java/com/dl/shop/payment/enums/PayEnums.java
@@ -8,7 +8,7 @@ public enum PayEnums {
 	PAY_RONGBAO_NOT_ENOUGH(304038,"提现金额超出提现数值"),
 	PAY_RONGBAO_BANK_ERROR(304039,"银行卡信息有误"),
 	PAY_RONGBAO_AMT_ERROR(304040,"请提供有效金额信息"),
-	PAY_RONGBAO_BANK_QUERY_ERROR(304041,"对不起，请选择有效的很行卡"),
+	PAY_RONGBAO_BANK_QUERY_ERROR(304041,"对不起，请选择有效的银行卡"),
 	PAY_CODE_BLANK(304042,"请提供有效支付方式"),
 	PAY_TOTAL_NOTRANGE(304043,"输入金额超出范围"),
 	CASH_REVIEWING(304044,"超出提现阈值,进入审核通道"),
