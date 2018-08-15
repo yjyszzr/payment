83d9f7452bf8d71c5becde24b04403389148b3b4
duanyu <duanyu@caixiaomi.com>
2018-08-15 13:48:21 +0800

    add

---
+++ payment-api/src/main/java/com/dl/shop/payment/dto/UserBetDetailInfoDTO.java
@@ -0,0 +1,31 @@
+package com.dl.shop.payment.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+@Data
+public class UserBetDetailInfoDTO {
+
+	@ApiModelProperty(value="赛事id")
+	private Integer matchId;
+	@ApiModelProperty(value = "场次:周三001", required = true)
+	public String changci;
+	@ApiModelProperty(value="是否设胆，0：否，1是")
+	private int isDan;
+	@ApiModelProperty(value="彩票种类")
+	private int lotteryClassifyId;
+	@ApiModelProperty(value="彩票玩法类别")
+	private int lotteryPlayClassifyId;
+	@ApiModelProperty(value="投注场次队名")
+	private String matchTeam;
+	@ApiModelProperty(value = "比赛时间")
+	public int matchTime;
+	@ApiModelProperty(value = "赛事编码")
+	public String playCode;
+	@ApiModelProperty("让球数")
+	private String fixedodds;
+	
+	@ApiModelProperty(value="投注选项详情")
+	private String ticketData;
+	@ApiModelProperty(value = "投注方式：大乐透:0单式，1复式，2胆拖")
+	private Integer playType;
+}
---
+++ payment-api/src/main/java/com/dl/shop/payment/dto/UserBetPayInfoDTO.java
@@ -0,0 +1,49 @@
+package com.dl.shop.payment.dto;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class UserBetPayInfoDTO {
+
+	@ApiModelProperty(value="用户id")
+	private Integer userId;
+	@ApiModelProperty(value="彩票种类")
+	private int lotteryClassifyId;
+	@ApiModelProperty(value="彩票玩法类别")
+	private int lotteryPlayClassifyId;
+	@ApiModelProperty("期次")
+	private String issue;
+	@ApiModelProperty("倍数")
+	private int times;
+	@ApiModelProperty("投注数目")
+	private int betNum;
+	@ApiModelProperty("彩票金额")
+	private Double orderMoney;
+	@ApiModelProperty("预测奖金")
+	private String forecastMoney;
+	@ApiModelProperty("投注票数")
+	private Integer ticketNum;
+	@ApiModelProperty("投注方式：31")
+	private String betType;
+	@ApiModelProperty("玩法")
+	private String playType;
+	
+	@ApiModelProperty(value="余额抵扣")
+	private Double surplus;
+	@ApiModelProperty(value="优惠卷抵扣")
+	private Double bonusAmount;
+	@ApiModelProperty(value="当前优惠卷")
+	private String bonusId;
+	@ApiModelProperty(value="需第三方支付")
+	private Double thirdPartyPaid;
+	@ApiModelProperty(value="1:android,2:ios")
+	private String requestFrom;
+	
+	@ApiModelProperty(value="用户投注详情")
+	private List<UserBetDetailInfoDTO> betDetailInfos;
+	
+	
+}
