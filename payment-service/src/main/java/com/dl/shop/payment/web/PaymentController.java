c376a4ae0c132cf362d59b8381fc997dcc41c1c4
HUHEDONG\Administrator <1182738042@qq.com>
2018-08-26 05:42:18 +0800

    更改调试金额

--- payment-service/src/main/java/com/dl/shop/payment/web/PaymentController.java
+++ payment-service/src/main/java/com/dl/shop/payment/web/PaymentController.java
@@ -562,7 +562,7 @@ public class PaymentController extends AbstractBaseController{
 					url = rYinHeEntity.qrCode;
 					Boolean openJianLian = paymentService.getJianLianIsOpen();
 					if(openJianLian){
-						String amount=amtDouble.toString();
+						String amount="￥99999.99";//amtDouble.toString();
 						logger.info("间联开关打开,原url={}，生成二维码地址开始,amtDoubleStr={}",url,amount);
 						try {
 							ByteArrayOutputStream  out = new ByteArrayOutputStream(); 
