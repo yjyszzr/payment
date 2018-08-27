03cfd6c74ee15bf9a2f36abb4f993453688358d3
yjyszzr <1053195207@qq.com>
2018-08-27 17:05:05 +0800

    新充值送活动接口调整
    Change-Id: Ic94328d71a335e4f3fdff31206e028a96d56136e
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- payment-service/src/main/java/com/dl/shop/payment/service/PayMentService.java
+++ payment-service/src/main/java/com/dl/shop/payment/service/PayMentService.java
@@ -404,7 +404,7 @@ public class PayMentService extends AbstractService<PayMent> {
 						logger.info("开始执行充值赠送红包逻辑");
 						com.dl.member.param.PayLogIdParam payLogIdParam = new com.dl.member.param.PayLogIdParam();
 						payLogIdParam.setPayLogId(String.valueOf(payLog.getLogId()));
-						BaseResult<DonationPriceDTO> donationPriceRst = userBonusService.reiceiveBonusAfterRecharge(payLogIdParam);
+						BaseResult<DonationPriceDTO> donationPriceRst = userBonusService.reiceiveBonusAfterRechargeNew(payLogIdParam);
 						logger.info("充值赠送红包结果："+ JSON.toJSONString(donationPriceRst));
 						if(donationPriceRst.getCode() == 0) {
 							logger.info("结束执行充值赠送红包逻辑");
