808f5b26c091cfbd3768e794fd9a65fb306d173d
liweijie <liweijie@caixiaomi.com>
2018-09-18 20:50:26 +0800

    提交 [配置文件

--- payment-service/src/main/java/com/dl/shop/payment/pay/tianxia/tianxiaScan/config/TXPayConfig.java
+++ payment-service/src/main/java/com/dl/shop/payment/pay/tianxia/tianxiaScan/config/TXPayConfig.java
@@ -14,28 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @ConfigurationProperties(prefix = "pay.tianxia")
 public class TXPayConfig {
-	// @Value("${pay.tianxia}")
 	private Map<String, String> merchant = new HashMap<String, String>();
-
-	private String DEBUG;
-	// 私钥
-	private String PRVKEY;
-	// 平台公钥
-	private String TXPUBKEY;
-	// md5
-	private String MD5KEY;
-	// 机构号
-	private String AGTID;
-	// 商户编号
-	private String MERID;
-	// 请求URL
-	private String REQ_URL;
-	// 回调函数
-	private String CALLBACK_URL;
-
-	// public void setMerchant(Map<String, String> merchant) {
-	// this.merchant = merchant;
-	// }
 
 	public String getDEBUG(String merchentNo) {
 		log.info("天下支付配置信息打印:={}", merchant);
