f180dc5e8d2d67a8f8717f721bbf5a189e9d5138
lifeilong <lifeilong@caixiaomi.com>
2019-06-24 17:30:25 +0800

    xg

--- payment-service/src/main/java/com/dl/shop/payment/pay/jhpay/util/HttpConfig.java
+++ payment-service/src/main/java/com/dl/shop/payment/pay/jhpay/util/HttpConfig.java
@@ -35,8 +35,6 @@ import com.alipay.api.response.AlipaySystemOauthTokenResponse;
  */
 public class HttpConfig {
 	private static final long serialVersionUID = 1L;
-	
-	private PayConfigUtil pcutil;
 	/**
 	 * 获取用户UserId
 	 */
@@ -90,70 +88,6 @@ public class HttpConfig {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
-		}
-	}
-	
-	@SuppressWarnings("finally")
-	public String doPostMessage(String reqUrl,SortedMap<String, String> map){
-		Map<String, String> params = SignUtils.paraFilter(map);
-		StringBuilder buf = new StringBuilder((params.size() + 1) * 10);
-		SignUtils.buildPayParams(buf, params, false);
-		String preStr = buf.toString();
-		String sign = MD5.sign(preStr, "&key=" + pcutil.getKEY(), "utf-8");
-		map.put("sign", sign);
-
-		reqUrl = pcutil.getPATH();
-		System.out.println("reqUrl：" + reqUrl);
-		CloseableHttpResponse response = null;
-		CloseableHttpClient client = null;
-		String res = null;
-		try {
-			HttpPost httpPost = new HttpPost(reqUrl);
-			StringEntity entityParams = new StringEntity(XmlUtils.parseXML(map), "utf-8");
-			httpPost.setEntity(entityParams);
-			client = HttpClients.createDefault();
-			response = client.execute(httpPost);
-			if (response != null && response.getEntity() != null) {
-				Map<String, String> resultMap = XmlUtils.toMap(EntityUtils.toByteArray(response.getEntity()), "utf-8");
-				res = XmlUtils.toXml(resultMap);
-				System.out.println("请求结果：" + res);
-
-				if (resultMap.containsKey("sign")) {
-					if (!SignUtils.checkParam(resultMap, pcutil.getKEY())) {
-						res = "验证签名不通过";
-					} else {
-						if ("0".equals(resultMap.get("status")) && "0".equals(resultMap.get("result_code"))) {
-							Map<String, String> orderResult = new HashMap<String, String>(); // 用来存储订单的交易状态(key:订单号，value:状态(0:未支付，1：已支付)) ---- 这里可以根据需要存储在数据库中
-							orderResult.put(map.get("out_trade_no"), "0");// 初始状态
-
-							String pay_info = resultMap.get("pay_info");
-							Map payInfo = JsonUtil.jsonToMap(pay_info);
-							String tradeNO = (String) payInfo.get("tradeNO");
-							String code_img_url = resultMap.get("code_img_url");
-						} else {
-							
-						}
-					}
-				}
-			} else {
-				res = "操作失败";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			res = "系统异常";
-		} finally {
-			System.out.println(res);
-			try {
-				if (response != null) {
-					response.close();
-				}
-				if (client != null) {
-					client.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return res;
 		}
 	}
 }
