001f66439d558a4f2954139cefa73849830ffea9
lifeilong <lifeilong@caixiaomi.com>
2019-07-08 16:08:26 +0800

    xg

--- payment-service/src/main/java/com/dl/shop/payment/service/JhPayService.java
+++ payment-service/src/main/java/com/dl/shop/payment/service/JhPayService.java
@@ -456,7 +456,7 @@ public class JhPayService {
 		JSONObject josn = (JSONObject) JSONObject.parse(payToken);
 		BigDecimal thirdPartyPaid = new BigDecimal(josn.getString("thirdPartyPaid"));
 		int paid = thirdPartyPaid.intValue();
-		if(paid>10000) {
+		if(paid>5000) {
 			return true;
 		}
 		return false;
